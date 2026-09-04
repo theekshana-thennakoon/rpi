88a785c8f4c4694469ae86288251178264268677
ITT2021_106 TheekshanaThennakoo <theekshanathennakoon79@gmail.com>
2026-09-04 11:31:29 +0530

    server updated

--- server.py
+++ server.py
@@ -9,7 +9,10 @@ import paramiko
 from flask import Flask, render_template, request, jsonify, Response, send_file
 from flask_cors import CORS
 
-app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+app = Flask(__name__,
+            template_folder=os.path.join(BASE_DIR, 'templates'),
+            static_folder=os.path.join(BASE_DIR, 'static'))
 CORS(app)
 
 # Global SSH configuration state
